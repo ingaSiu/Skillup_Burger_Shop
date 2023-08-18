abf3fe2517b061f72ad88da88641c22e96350423
ingaSiu <ingasiud@gmail.com>
2023-08-18 19:55:29 +0300

    fix comments

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
-import Contact from "./components/contact/Contact";
+import './styles/app.scss';
+import './styles/header.scss';
+import './styles/home.scss';
+import './styles/founder.scss';
+import './styles/menu.scss';
+import './styles/footer.scss';
+import './styles/contact.scss';
+import './styles/cart.scss';
+import './styles/shipping.scss';
+import './styles/login.scss';
+import './styles/profile.scss';
+import './styles/table.scss';
+import './styles/orderDetails.scss';
+import './styles/about.scss';
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
+import OrderDetails from './components/myOrders/OrderDetails';
+import Profile from './components/profile/Profile';
+
 // Add module imports for CART
 
 // Add module imports for Shipping
 
-import Login from "./components/login/Login";
-import Profile from "./components/profile/Profile";
 // Add module imports for MY ORDERS
-
-import OrderDetails from "./components/myOrders/OrderDetails";
-import About from "./components/about/About";
-
-
-import "./styles/app.scss";
-import "./styles/header.scss";
-import "./styles/home.scss";
-import "./styles/founder.scss";
-import "./styles/menu.scss";
-import "./styles/footer.scss";
-import "./styles/contact.scss";
-import "./styles/cart.scss";
-import "./styles/shipping.scss";
-import "./styles/login.scss";
-import "./styles/profile.scss";
-import "./styles/table.scss";
-import "./styles/orderDetails.scss";
-import "./styles/about.scss";
 
 function App() {
   return (
@@ -38,19 +38,15 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-          //           Add the Route for CART
+        {/* //           Add the Route for CART
       
-          //           Add the Route for SHIPPING
-        
+          //           Add the Route for SHIPPING */}
+
         <Route path="/login" element={<Login />} />
         <Route path="/me" element={<Profile />} />
-         //           Add the Route for MY ORDERS
+        {/* //           Add the Route for MY ORDERS */}
 
         <Route path="/order/:id" element={<OrderDetails />} />
-      
-       
-
-        
       </Routes>
 
       <Footer />
