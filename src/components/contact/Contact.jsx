39852365a4ea4d1e6e7765ec341b95514cd2845b
ingaSiu <ingasiud@gmail.com>
2023-08-24 09:58:37 +0300

    task

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,21 +1,87 @@
 // Write all the code here
 
-import Popup from 'reactjs-popup';
+import axios from 'axios';
 import burger from '../../assets/burger2.png';
+import { toast } from 'react-hot-toast';
+import { useState } from 'react';
+
+const CONTACT_API_URL = 'https://testapi.io/api/otakuneko/resource/ibmform';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('');
+
+  const emailRegex = /^([a-zA-Z0-9._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+
+  const sendEmail = () => {
+    try {
+      axios
+        .post(
+          CONTACT_API_URL,
+          { name: name, email: email, message: message },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        )
+        .then(() => {
+          setStatus('success');
+        });
+    } catch (err) {
+      console.log('error');
+      setStatus('error');
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email.length === 0) {
+      setStatus('empty');
+    } else if (!email.match(emailRegex)) {
+      setStatus('invalid');
+    } else {
+      sendEmail();
+      setStatus('');
+      setEmail('');
+      setMessage('');
+      setName('');
+    }
+  };
+
   return (
     <section className="contact">
-      <form>
+      {status === 'error' && toast.error('Request failed. Please try again')}
+
+      {status === 'empty' && toast.error('Please enter your email.')}
+
+      {status === 'invalid' && toast.error('Invalid email. Please enter a valid email, e.g. sample@mail.com')}
+
+      {status === 'success' && toast.success('Thank you for contacting us! We will reach out to you shortly.')}
+
+      <form onSubmit={handleSubmit}>
         <h2>Contact Us</h2>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Your Email" required />
+        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
+        <input
+          type="email"
+          placeholder="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
-        <textarea placeholder="Your Message" cols={40} rows={10}></textarea>
+        <textarea
+          placeholder="Your Message"
+          cols={40}
+          rows={10}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        ></textarea>
 
-        <Popup trigger={<button type="submit">Send</button>}>
-          <div className="popupText">Thank you for contacting us! We will reach out to you shortly.</div>
-        </Popup>
+        <button>Send</button>
       </form>
 
       <div className="formBorder">
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <Toaster position="top-center" />
+  </React.StrictMode>,
 );
