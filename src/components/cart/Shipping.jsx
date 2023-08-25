f768458db095d0309374267b0be0effc9b101c75
ingaSiu <ingasiud@gmail.com>
2023-08-25 18:58:34 +0300

    add shipping details

--- src/components/cart/Shipping.jsx
+++ src/components/cart/Shipping.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Country, State } from "country-state-city";
+import { Country, State } from 'country-state-city';
+
 import Popup from 'reactjs-popup';
+import React from 'react';
 
 const Shipping = () => {
   return (
@@ -17,32 +18,61 @@ const Shipping = () => {
             <input type="text" placeholder="Enter City" />
           </div>
           <div>
-              {/* Compelte the code for the COUNTRY DROPDOWN*/}
+            {/* Compelte the code for the COUNTRY DROPDOWN*/}
+
             <label>Country</label>
 
             <select>
               <option value="">Country</option>
-// Enter the code here for country dropdown           
+              {/* // Enter the code here for country dropdown    */}
+              {Country &&
+                Country.getAllCountries().map((item) => (
+                  <option value={item.isoCode} key={item.isoCode}>
+                    {' '}
+                    {item.name}{' '}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <div>
+            <label>State</label>
+            <select>
+              <option value="">State</option>
+              {State &&
+                State.getStatesOfCountry('IN').map((item) => (
+                  <option value={item.isoCode} key={item.isoCode}>
+                    {item.name}
                   </option>
                 ))}
             </select>
           </div>
           <div>
-              {/* Add the code for the STATE DROPDOWN*/}
-           
-          </div>
-          <div>
             <label>Pin Code</label>
             <input type="number" placeholder="Enter Pincode" />
           </div>
-        // Enter thr code for contact           
-          
-          <Popup trigger=
-                {<button type = "button">Confirm Order</button>}
-                position="right center">
-                <div style={{color:"red",position: 'absolute', top: '50%', right: '100%', transform: 'translateY(-50%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Order Placed</div>
-               
-            </Popup>
+          {/* // Enter thr code for contact */}
+          <div>
+            <label>Phone Number</label>
+            <input type="number" placeholder="Enter Phone Number" />
+          </div>
+          <Popup trigger={<button type="button">Confirm Order</button>} position="right center">
+            <div
+              style={{
+                color: 'red',
+                position: 'absolute',
+                top: '50%',
+                right: '100%',
+                transform: 'translateY(-50%)',
+                backgroundColor: '#fff',
+                padding: '10px',
+                borderRadius: '5px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+              }}
+            >
+              Order Placed
+            </div>
+          </Popup>
         </form>
       </main>
     </section>
